141a8b1403a2697c8742187964113cb0ce20b390
Pascal Zumkehr <spam@codez.ch>
2012-11-29 17:41:55 +0100

    add template override test

--- test/dummy/vendor/wagons/superliner/test/functionals/people_controller_test.rb
+++ test/dummy/vendor/wagons/superliner/test/functionals/people_controller_test.rb
@@ -9,6 +9,8 @@ class PeopleControllerTest < ActionController::TestCase
     assert_template 'sidebar'
     assert_template 'sidebar_superliner'
     assert_match /42/, @response.body
+    assert_match /Superliner Details/, @response.body
+    assert_no_match /Main Details/, @response.body
   end
   
 end
