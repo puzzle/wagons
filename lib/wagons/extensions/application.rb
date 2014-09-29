015b4e1ee60018fa8175c33c8bb505d876401e6f
Pascal Zumkehr <spam@codez.ch>
2014-09-29 16:02:50 +0200

    fix #ordered_railties for rails 4.1.6

--- lib/wagons/extensions/application.rb
+++ lib/wagons/extensions/application.rb
@@ -6,11 +6,25 @@ module Rails
     def ordered_railties_with_wagons
       @ordered_railties ||= ordered_railties_without_wagons.tap do |ordered|
         Wagons.all.each do |w|
-          ordered.push(ordered.delete(w))
+          if Rails::VERSION::STRING < '4.1.6'
+            ordered.push(ordered.delete(w))
+          else
+            ordered.unshift(array_deep_delete(ordered, w))
+          end
         end
       end
     end
-
     alias_method_chain :ordered_railties, :wagons
+
+    private
+
+    def array_deep_delete(array, item)
+      array.delete(item) ||
+      array.select  { |i| i.is_a?(Array) }.
+            collect { |i| array_deep_delete(i, item) }.
+            compact.
+            first
+    end
+
   end
 end
