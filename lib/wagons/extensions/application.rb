5f12aaf626c87fc0743922083a931d39ce368fa0
Pascal Zumkehr <spam@codez.ch>
2015-07-22 17:36:51 +0200

    fix version comparison

--- lib/wagons/extensions/application.rb
+++ lib/wagons/extensions/application.rb
@@ -5,8 +5,9 @@ module Rails
     # Append wagons at the end of all railties, even after the application.
     def ordered_railties_with_wagons
       @ordered_railties ||= ordered_railties_without_wagons.tap do |ordered|
+        flat = Gem::Version.new(Rails::VERSION::STRING) < Gem::Version.new('4.1.6')
         Wagons.all.each do |w|
-          if Rails::VERSION::STRING < '4.1.6'
+          if flat
             ordered.push(ordered.delete(w))
           else
             ordered.unshift(array_deep_delete(ordered, w))
